cd11437923b6c686733f6e3297e492e6b9e9560d
kaushik yadav <kaushikyadavjallam005@gmail.com>
2021-03-11 20:17:38 +0530

    switching between tabs

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import {Route} from 'react-router-dom'
 import axios from '../../axios'
 import Posts from '../Blog/Posts/Posts'
+import NewPost from './NewPost/NewPost'
 import './Blog.css';
 
 
@@ -24,6 +25,7 @@ class Blog extends Component {
                     </nav>
                 </header>
                <Route path='/' exact component={Posts}/>
+               <Route path='/new-post' component={NewPost} />
                
             </div>
         );
